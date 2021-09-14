d53aadecc83069f8215b371c98bb396e8b50305d
Black1978 <chernyi.andrei@gmail.com>
2021-09-14 11:55:28 +0200

    The srver fetching is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import Card, { CardVariant } from './components/Card'
 import UserList from './components/UserList'
 import { IUser } from './types/types'
 const App = () => {
    const [users, setUsers] = useState<IUser[]>([])
+   useEffect (() => {
+       fetchUsers()
+   }, [])
+   async function fetchUsers() {
+       try {
+          const responce =  await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+          setUsers(responce.data)
+           
+       } catch (error) {
+           alert(error)
+           
+       }
+       
+   }
     return (
         <div>
             <Card
